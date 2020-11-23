d971654e8c162e1e3308972b6289feb25107fa41
Kiung Kim <kirino9024@naver.com>
2020-11-24 08:28:11 +0900

    fix: remove onUpdated & hide instantOrder

--- content-script.js
+++ content-script.js
@@ -10,5 +10,8 @@ const removeSellTab = (state) => {
     chrome.runtime.sendMessage({ state: state })
 
     const sellTab = document.querySelector('.top > .tabB__sell')
+    const instantTab = document.querySelector('.top > .tabB__instantOrder')
+
     state ? sellTab.style.display = '' : sellTab.style.display = 'none'
+    state ? instantTab.style.display = '' : instantTab.style.display = 'none'
 }
